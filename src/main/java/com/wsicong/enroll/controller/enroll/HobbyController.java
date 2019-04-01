8ee2397325e447474da24f14c09ebe26648d3398
Wsicong <wsicong@foxmail.com>
2019-04-01 17:22:30 +0800

    权限管理完成，开始兴趣班管理

---
+++ src/main/java/com/wsicong/enroll/controller/enroll/HobbyController.java
@@ -0,0 +1,17 @@
+package com.wsicong.enroll.controller.enroll;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 兴趣班管理Controller
+ */
+@Controller
+@RequestMapping("/hobby")
+public class HobbyController {
+    private static final Logger logger = LoggerFactory.getLogger(HobbyController.class);
+
+
+}
